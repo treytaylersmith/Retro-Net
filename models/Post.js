af7f1fc6086fd22ef922f9d0ad4f3d0ac1807ef9
Trey Tayler Smith <treytaylersmith@gmail.com>
2024-09-05 21:20:35 -0500

    models created, user routes created

---
+++ models/Post.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("..config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    text: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      references: {
+        model: "user",
+        key: "name",
+      },
+    },
+    comments: {
+      type: DataTypes.ARRAY(DataTypes.JSON),
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+module.exports = Post;
