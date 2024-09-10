27d3651a4c2e74330c296d2bcf24b7c438bf4dc8
Trey Tayler Smith <treytaylersmith@gmail.com>
2024-09-09 19:36:56 -0500

    added CRUD routes for postRoutes, added server.js file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./posts');
+const postRoutes = require('./postsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
