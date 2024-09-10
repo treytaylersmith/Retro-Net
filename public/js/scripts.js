b33037d3e60b90925baf4015a3b922407d2cb418
bebo1719 <142172190+bebo1719@users.noreply.github.com>
2024-09-09 19:13:12 -0500

    front

---
+++ public/js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Retro Net Frontend Loaded');
+    
+    // Add any interactive JavaScript functionality here, such as form validation
+});
